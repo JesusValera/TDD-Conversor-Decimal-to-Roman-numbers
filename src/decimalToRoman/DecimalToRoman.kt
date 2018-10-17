1830bd8cb897f51dfcf363db378f75e09d0745bb
Jesus Valera <jesus1902@outlook.com>
2018-10-17 19:27:17 +0200

    Converting numbers from -n to 1000. Also in recursive mode.

--- src/decimalToRoman/DecimalToRoman.kt
+++ src/decimalToRoman/DecimalToRoman.kt
@@ -6,12 +6,34 @@ class DecimalToRoman {
 
     private var result: String = ""
 
+    fun convertRecursive(number: Int, result: String = ""): String = when {
+        (0 > number) -> throw NegativeNumber()
+        (1000 <= number) -> convertRecursive(number - 1000, result + "M")
+        (900 <= number) -> convertRecursive(number - 900, result + "CM")
+        (500 <= number) -> convertRecursive(number - 500, result + "D")
+        (400 <= number) -> convertRecursive(number - 400, result + "CD")
+        (100 <= number) -> convertRecursive(number - 100, result + "C")
+        (90 <= number) -> convertRecursive(number - 90, result + "XC")
+        (50 <= number) -> convertRecursive(number - 50, result + "L")
+        (40 <= number) -> convertRecursive(number - 40, result + "XL")
+        (10 <= number) -> convertRecursive(number - 10, result + "X")
+        (9 <= number) -> convertRecursive(number - 9, result + "IX")
+        (5 <= number) -> convertRecursive(number - 5, result + "V")
+        (4 <= number) -> convertRecursive(number - 4, result + "IV")
+        (1 <= number) -> convertRecursive(number - 1, result + "I")
+        else -> result
+    }
+
     fun convert(_number: Int): String {
-        var number = _number
-        result = ""
+        var number = _number // val cannot be reassigned.
+        formatResult()
 
-        if (number < 0) throw NegativeNumber()
+        isNegativeNumber(number)
 
+        number = subtractAmount(1000, number, "M")
+        number = subtractAmount(900, number, "CM")
+        number = subtractAmount(500, number, "D")
+        number = subtractAmount(400, number, "CD")
         number = subtractAmount(100, number, "C")
         number = subtractAmount(90, number, "XC")
         number = subtractAmount(50, number, "L")
@@ -23,6 +45,14 @@ class DecimalToRoman {
         subtractAmount(1, number, "I")
 
         return result
+    }
+
+    private fun formatResult() {
+        result = ""
+    }
+
+    private fun isNegativeNumber(number: Int) {
+        if (0 > number) throw NegativeNumber()
     }
 
     private fun subtractAmount(subtract: Int, _number: Int, romanNumber: String): Int {
--- src/decimalToRoman/DecimalToRomanTest.kt
+++ src/decimalToRoman/DecimalToRomanTest.kt
@@ -9,7 +9,7 @@ class DecimalToRomanTest {
     private val decimalToRoman = DecimalToRoman()
 
     @Test
-    fun lessToZero() {
+    fun lessThanZero() {
         assertThrows<DecimalToRoman.NegativeNumber> { decimalToRoman.convert(-1) }
     }
 
@@ -43,6 +43,67 @@ class DecimalToRomanTest {
         assertEquals("LXXX", decimalToRoman.convert(80))
         assertEquals("XC", decimalToRoman.convert(90))
         assertEquals("C", decimalToRoman.convert(100))
+    }
+
+    @Test
+    fun convertOneHundredToOneThousandAndSoOn() {
+        assertEquals("CC", decimalToRoman.convert(200))
+        assertEquals("CD", decimalToRoman.convert(400))
+        assertEquals("D", decimalToRoman.convert(500))
+        assertEquals("DC", decimalToRoman.convert(600))
+        assertEquals("DCCC", decimalToRoman.convert(800))
+        assertEquals("CM", decimalToRoman.convert(900))
+        assertEquals("M", decimalToRoman.convert(1000))
+        assertEquals("MM", decimalToRoman.convert(2000))
+    }
+
+    @Test
+    fun lessThanZeroRecursive() {
+        assertThrows<DecimalToRoman.NegativeNumber> { decimalToRoman.convertRecursive(-1) }
+    }
+
+    @Test
+    fun equalToZeroRecursive() {
+        assertEquals("", decimalToRoman.convertRecursive(0))
+    }
+
+    @Test
+    fun convertOneToTenRecursive() {
+        assertEquals("I", decimalToRoman.convertRecursive(1))
+        assertEquals("II", decimalToRoman.convertRecursive(2))
+        assertEquals("III", decimalToRoman.convertRecursive(3))
+        assertEquals("IV", decimalToRoman.convertRecursive(4))
+        assertEquals("V", decimalToRoman.convertRecursive(5))
+        assertEquals("VI", decimalToRoman.convertRecursive(6))
+        assertEquals("VII", decimalToRoman.convertRecursive(7))
+        assertEquals("VIII", decimalToRoman.convertRecursive(8))
+        assertEquals("IX", decimalToRoman.convertRecursive(9))
+        assertEquals("X", decimalToRoman.convertRecursive(10))
+    }
+
+    @Test
+    fun convertTenToOneHundredRecursive() {
+        assertEquals("XX", decimalToRoman.convertRecursive(20))
+        assertEquals("XXIII", decimalToRoman.convertRecursive(23))
+        assertEquals("XXIV", decimalToRoman.convertRecursive(24))
+        assertEquals("XXXIX", decimalToRoman.convertRecursive(39))
+        assertEquals("XL", decimalToRoman.convertRecursive(40))
+        assertEquals("L", decimalToRoman.convertRecursive(50))
+        assertEquals("LXXX", decimalToRoman.convertRecursive(80))
+        assertEquals("XC", decimalToRoman.convertRecursive(90))
+        assertEquals("C", decimalToRoman.convertRecursive(100))
+    }
+
+    @Test
+    fun convertOneHundredToOneThousandAndSoOnRecursive() {
+        assertEquals("CC", decimalToRoman.convertRecursive(200))
+        assertEquals("CD", decimalToRoman.convertRecursive(400))
+        assertEquals("D", decimalToRoman.convertRecursive(500))
+        assertEquals("DC", decimalToRoman.convertRecursive(600))
+        assertEquals("DCCC", decimalToRoman.convertRecursive(800))
+        assertEquals("CM", decimalToRoman.convertRecursive(900))
+        assertEquals("M", decimalToRoman.convertRecursive(1000))
+        assertEquals("MM", decimalToRoman.convertRecursive(2000))
     }
 
 }
